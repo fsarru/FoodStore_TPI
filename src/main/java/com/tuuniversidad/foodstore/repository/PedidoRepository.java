5d684a38fe8ecfcffa094fc070ffb12a807948ca
fsarru <francosarru@gmail.com>
2026-06-10 22:56:22 -0300

    Adaptación a la nueva rúbrica del TP

--- src/main/java/com/tuuniversidad/foodstore/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tuuniversidad.foodstore.repository;
-
-import com.tuuniversidad.foodstore.entities.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
